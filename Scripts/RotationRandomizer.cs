44900971d99b550f1861c4a270b880c1d93c1749
239developer <factoriogmer@gmail.com>
2021-01-18 16:56:50 +0300

    Added coins with random Y rotation

---
+++ Scripts/RotationRandomizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationRandomizer : MonoBehaviour
+{
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(0f, Random.value * 360, 0f);
+    }
+}
