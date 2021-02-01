1b6ec5098ddc39ec225584910f7d8f3fcb4d7894
239developer <factoriogmer@gmail.com>
2021-02-01 17:54:39 +0300

    working on tuto

---
+++ Scripts/Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject[] text1;
+    private byte counter = 0;
+    private float lastAction = 0f;
+
+    void SwitchArray(GameObject[] array, bool state)
+    {
+        for(int i = 0; i < array.Length; i++)
+            array.SetActive(state);
+    }
+
+    void Start()
+    {
+        SwitchArray(text1, true);
+    }
+
+    void Update()
+    {
+        if(Time.time - lastAction > 0.5f)
+            switch(counter)
+            {
+                case 0:
+                    SwitchArray(text1, false);
+                    break;
+            }
+    }
+}
