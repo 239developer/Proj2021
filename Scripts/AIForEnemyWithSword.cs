673ce29a0f86ca47ecb5dc760c7c0a034b92c0c4
x613core <serggorkov@icloud.com>
2021-02-01 16:51:12 +0300

    доделал всё связанное со вторым врагом

--- Scripts/AIForEnemyWithSword.cs
+++ Scripts/AIForEnemyWithSword.cs
@@ -8,18 +8,18 @@ public class AIForEnemyWithSword : MonoBehaviour
 	public float speedd = 20.0f; 
 	public float shootdistase = 3.0f;
 	void Update() { 
-		transform.Rotate(0, speedd * Time.deltaTime, 0);
 		Ray ray = new Ray(transform.position, transform.forward); 
 		RaycastHit hit;
 		if (Physics.SphereCast(ray, 0.75f, out hit)) { 
 			GameObject hitObject = hit.transform.gameObject;
 			if (hitObject.GetComponent<PlayerCharacter>()) {
-				speedd = 0.0f;
 				if (hit.distance > shootdistase) { 
 					transform.Translate(0, 0, speed * Time.deltaTime);
+				}else{
+
 				}
 			}else{
-				speedd = 20.0f;
+				transform.Rotate(0, speedd * Time.deltaTime, 0);
 			}	
 		}
 	}
